84c9a022e8b4b9db581b69db1457042714e80594
hasee <wgf82981599@163.com>
2025-12-12 14:09:49 +0800

    苍穹外卖初始代码

---
+++ sky-common/src/main/java/com/sky/properties/JwtProperties.java
@@ -0,0 +1,26 @@
+package com.sky.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "sky.jwt")//配置文件中所有以sky.jwt开头的配置项，会自动映射到该类的同名属性中
+@Data
+public class JwtProperties {
+
+    /**
+     * 管理端员工生成jwt令牌相关配置
+     */
+    private String adminSecretKey;
+    private long adminTtl;
+    private String adminTokenName;
+
+    /**
+     * 用户端微信用户生成jwt令牌相关配置
+     */
+    private String userSecretKey;
+    private long userTtl;
+    private String userTokenName;
+
+}
